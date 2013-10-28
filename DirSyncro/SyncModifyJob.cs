6262660d583f60a1bd1851382586f02e66230527
mjonsson <smjonsson76@me.com>
2013-10-28 21:50:57 +0100

    Dev

--- DirSyncro/SyncModifyJob.cs
+++ DirSyncro/SyncModifyJob.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace DirSyncro
 {
-    class SyncModifyJob
+    class SyncModifyJob : SyncJob
     {
         public SyncModifyJob()
         {
@@ -21,40 +22,37 @@ namespace DirSyncro
             try
             {
                 DirectoryInfo fullTarget = null;
-
-                if (syncMessage.sourceFile.Exists) { 
-                    syncMessage.partialPath = syncMessage.sourceFile.ToString().Replace(syncMessage.sourcePath.ToString(), syncMessage.targetPath.ToString())
-                        fullTarget = new FileInfo(syncMessage.targetPath + "\\" + sourceName).Directory;
-                //        CreatePath(fullTarget);
-
-                //        string searchPattern = String.Format("{0}\\{1}_*{2}", fullTarget.FullName, Path.GetFileNameWithoutExtension(sourceFull), Path.GetExtension(sourceFull));
-                //        FileInfo[] fileVersions = fullTarget.GetFiles(searchPattern, SearchOption.TopDirectoryOnly);
-
-                //        if (fileVersions.Length > versions)
-                //        {
-                //            // Sort array so that the oldes file is first in list
-                //            Array.Sort(fileVersions, (f1, f2) => f1.Name.CompareTo(f2.Name));
-
-                //            for (int i = 0; i < fileVersions.Length - versions + 1; i++)
-                //            {
-                //                fileVersions[i].Delete();
-                //            }
-                //        }
-
-                //        // Construct filename with timestamp
-                //        String newFullTarget = String.Format("{0}\\{1}_{2}{3}", fullTarget.FullName, Path.GetFileNameWithoutExtension(sourceFull), CreateTimestamp(), Path.GetExtension(sourceFull));
-                //        File.Copy(sourceFull, newFullTarget, false);
-                //    }
+                if (string.IsNullOrEmpty(syncMessage.partialPath))
+                {
+                    fullTarget = new DirectoryInfo(syncMessage.targetPath);
                 }
+                else
+                {
+                    fullTarget = new DirectoryInfo(syncMessage.targetPath + "\\" + syncMessage.partialPath);
+                }
+
+                CreatePath(fullTarget);
+                FileInfo[] fileVersions = fullTarget.GetFiles(String.Format("{0}_*{2}", Path.GetFileNameWithoutExtension(syncMessage.sourceFile.Name), Path.GetExtension(syncMessage.sourceFile.Name)), SearchOption.TopDirectoryOnly);
+
+                if (fileVersions.Length > syncMessage.versions)
+                {
+                    // Sort array so that the oldes file is first in list
+                    Array.Sort(fileVersions, (f1, f2) => f1.Name.CompareTo(f2.Name));
+
+                    for (int i = 0; i < fileVersions.Length - syncMessage.versions + 1; i++)
+                    {
+                        fileVersions[i].Delete();
+                    }
+                }
+
+                // Construct filename with timestamp
+                String newFullTarget = String.Format("{0}\\{1}_{2}{3}", fullTarget.FullName, Path.GetFileNameWithoutExtension(syncMessage.sourceFile.Name), CreateEpoch(), Path.GetExtension(syncMessage.sourceFile.Name));
+                File.Copy(syncMessage.sourceFile.FullName, newFullTarget, false);
             }
             catch (IOException ex)
             {
                 Console.WriteLine(ex.StackTrace);
             }
-
-            //Console.WriteLine(String.Format("Thread: {2}, Event: {0}, Fullpath: {1}", job.eventType, job.sourceFull, Thread.CurrentThread.ManagedThreadId.ToString()));
-
-            //job.finished = true;
         }
     }
 }
